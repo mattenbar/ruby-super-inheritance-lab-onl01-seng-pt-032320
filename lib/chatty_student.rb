8c1ad121831d68ef36661661cfdd1c4933ee6b44
matthew enbar <matthew.enbar@gmail.com>
2020-04-09 16:20:35 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,7 +1,8 @@
 class ChattyStudent < Student
 
   def hello
-    super + "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last n
+    puts super
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last n
 ight? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
 
